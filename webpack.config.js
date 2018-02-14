e6a13a8d06f1a1035a5af822abed0e07f91ce8e6
Paulão Developer <jskhanframework@gmail.com>
2018-02-13 23:46:09 -0200

    'novo'

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,55 @@
-var path = require('path');
-var webpack = require('webpack');
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const path = require('path');
+var LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+
+const IS_PROD = (process.env.NODE_ENV === 'production');
 
 module.exports = {
-     watch: true,
-     entry: [
-         "babel-polyfill", 
-         './vendor/app/app.js'
-    ],
-     output: {
-         path: path.resolve(__dirname, 'vendor', 'build'),
-         filename: 'khan.min.js'
-     },
-     module: {
-		loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader", // or just "babel"
-                query: {
-                    presets: ["env", "es2015"]
-                }
-            }
-        ]
-	 },
-     stats: {
-         colors: true
-     },
-     devtool: 'source-map'
+  watch: true,
+  entry: {
+    'index': path.join(__dirname, 'src', 'index.js')
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  resolve: {
+    modules: [ 'node_modules', 'src' ],
+    extensions: [".js", ".jsx", ".scss", ".css", ".html"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: [/node_modules/],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: ['lodash'],
+            presets: ['es2015', 'react'],
+            comments: false,
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      server: {
+        baseDir: ['./']
+      }
+    }),
+    new webpack.DefinePlugin({
+      PRODUCTION: JSON.stringify(IS_PROD)
+    }),
+    new webpack.ProvidePlugin({
+      axios: "axios"
+    })
+  ],
+  devtool: "source-map"
 };
